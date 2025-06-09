938bb7c52efbfafe9da655ac1bbc40d004de0662
anumukonda-siri-hasini <24eu08136@vrsec.ac.in>
2025-06-09 13:45:53 +0530

    Create products.js

---
+++ products.js
@@ -0,0 +1,32 @@
+const products = [
+  {
+    id: 1,
+    name: "Classic Black Sunglasses",
+    price: 1299,
+    img: "https://images.unsplash.com/photo-1504203704997-21016f5c0b84?auto=format&fit=crop&w=400&q=80"
+  },
+  {
+    id: 2,
+    name: "Retro Round Glasses",
+    price: 1599,
+    img: "https://images.unsplash.com/photo-1516594798945-dc08e6e0b0e6?auto=format&fit=crop&w=400&q=80"
+  },
+  {
+    id: 3,
+    name: "Aviator Shades",
+    price: 1899,
+    img: "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?auto=format&fit=crop&w=400&q=80"
+  },
+  {
+    id: 4,
+    name: "Square Frame Glasses",
+    price: 999,
+    img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?auto=format&fit=crop&w=400&q=80"
+  },
+  {
+    id: 5,
+    name: "Sporty Wrap Glasses",
+    price: 1399,
+    img: "https://images.unsplash.com/photo-1526045612212-70caf35c14df?auto=format&fit=crop&w=400&q=80"
+  }
+];
